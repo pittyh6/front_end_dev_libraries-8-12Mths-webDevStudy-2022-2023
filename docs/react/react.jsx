e23d335b5f1a6337003cb8614a7f6184abe8467a
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-01-02 17:10:45 +1100

    react - add comment and update readme

--- docs/react/react.jsx
+++ docs/react/react.jsx
@@ -1,17 +1,27 @@
 // 1 ---------
 const hello = <h1>Hello JSX!</h1>;
 
-
-
 // 2 ---------
-const JSX = (<div>
+const JSX = (
+  <div>
     <h1>H1 Text</h1>
     <p>Paragraph Text</p>
     <ul>
-        <li>List 1</li>
-        <li>List 2</li>
-        <li>List 3</li>
+      <li>List 1</li>
+      <li>List 2</li>
+      <li>List 3</li>
     </ul>
-</div>);
+  </div>
+);
 
 // 3 ---------
+{
+    const JSX = (
+        <div>
+          {/*comment */}
+          <h1>This is a block of JSX</h1>
+          <p>Here's a subtitle</p>
+        </div>
+      );
+}
+
